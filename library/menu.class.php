16867a4a7e6f120a268063980a2a314be7ec8dd5
profoundblue <profoundblue@163.com>
2017-04-19 16:00:23 +0800

    0003++

--- library/menu.class.php
+++ library/menu.class.php
@@ -6,11 +6,27 @@ class Menu {
 	
 	/**
 	 */
-	protected $domain;
-	protected $accessToken;
+	const media_all = 'media_id';
+	const media_text = 'view_limited';
+	const click_custom = 'click';
+	const click_scan_push = 'scancode_push';
+	const click_scan_waiting = 'scancode_waiting';
+	const click_camera = 'pic_sysphoto';
+	const click_camera_photo = 'pic_photo_or_album';
+	const click_wx_photo = 'pic_weixin';
+	const click_location = 'location_select';
 	
 	/**
-	 * public void function __construct(string $token, ?string $domain = null)
+	 */
+	protected $domain;
+	protected $accessToken;
+	protected $menus=array();
+	
+	/**
+	 *
+	 * @param string $token        	
+	 * @param ?string $domain = null
+	 * @return void
 	 */
 	public function __construct(string $token, string $domain = null) {
 		$this->domain = is_null($domain) ? 'api.weixin.qq.com' : $domain;
@@ -18,23 +34,117 @@ class Menu {
 	}
 	
 	/**
-	 * public void function __destruct(void)
+	 * @reutrn void
 	 */
 	function __destruct() {
 		//
 	}
 	
 	/**
-	 * public boolean function delete(void)
+	 *
+	 * @param string $name        	
+	 * @param string $url        	
+	 * @return array
+	 */
+	public function createView(string $name, string $url): array {
+		return array(
+			'type'=>'view', 
+			'name'=>$name, 
+			'url'=>$url
+		);
+	}
+	
+	/**
+	 * public array function createClick(string $name, string $key, string $type = self::click_custom)
+	 */
+	public function createClick(string $name, string $key, string $type = self::click_custom): array {
+		return array(
+			'type'=>$type, 
+			'name'=>$name, 
+			'key'=>$key
+		);
+	}
+	
+	/**
+	 *
+	 * @param string $name        	
+	 * @param string $id        	
+	 * @param string $type        	
+	 * @return array
+	 */
+	public function createMedia(string $name, string $id, string $type = self::media_all): array {
+		return array(
+			'type'=>$type, 
+			'name'=>$name, 
+			'media_id'=>$id
+		);
+	}
+	
+	/**
+	 *
+	 * @param string $name        	
+	 * @param string $url        	
+	 * @param string $appId        	
+	 * @param string $pagePath        	
+	 * @return array
+	 */
+	public function createMiniProgram(string $name, string $url, string $appId, string $pagePath): array {
+		return array(
+			'type'=>'miniprogram', 
+			'name'=>$name, 
+			'url'=>$url, 
+			'appid'=>$appId, 
+			'pagepath'=>$pagePath
+		);
+	}
+	
+	/**
+	 * 
+	 * @param array $menu
+	 * @param array $button
+	 * @return array
+	 */
+	public function addSubMenu(array $menu,array $button):array {
+		if(!isset($menu['sub_button'])) $menu['sub_button']=array();
+		$menu['sub_button'][]=$button;
+		return $menu;
+	}
+	
+	/**
+	 * 
+	 * @param array $button
+	 * @return array
+	 */
+	public function addMenu(array $button):array{
+		if(!isset($this->menus['button'])) $this->menus['button']=array();
+		$this->menus['button'][]=$button;
+		return $button;
+	}
+	
+	/**
+	 * 
+	 */
+	public function create():bool{
+		$translator=new Translator();
+		$json=$translator->createJSON($this->menus);
+		$mimicry=new Mimicry();
+		$end=$mimicry->post($url, array('body'=>$json));
+	}
+	
+	/**
+	 * @return boolean
 	 */
 	public function delete(): bool {
-		$url='https://'.$this->domain.'/cgi-bin/menu/delete';
-		$qryStrings=array('access_token'=>$this->accessToken);
-		$mm=new Mimicry();
-		$json=$mm->get($url, $qryStrings);
-		$ends=json_decode($json);
-		return 'ok'==$ends['errmsg'] ? true : false;
+		$url = 'https://' . $this->domain . '/cgi-bin/menu/delete';
+		$qryStrings = array(
+			'access_token'=>$this->accessToken
+		);
+		$mm = new Mimicry();
+		$json = $mm->get($url, $qryStrings);
+		$ends = json_decode($json);
+		return 'ok' == $ends['errmsg'] ? true : false;
 	}
+	
 	//
 }
 
--- library/translator.class.php
+++ library/translator.class.php
@@ -2,6 +2,9 @@
 // declare(strict_types = 1);
 namespace SporeAura\WX;
 
+use DOMDocument;
+use DOMElement;
+
 class Translator {
 	
 	/**
@@ -12,7 +15,7 @@ class Translator {
 	}
 	
 	/**
-	 * public void function __destruct(void) 
+	 * public void function __destruct(void)
 	 */
 	function __destruct() {
 		//
@@ -21,9 +24,9 @@ class Translator {
 	/**
 	 * public string function createJSON(array $datas)
 	 */
-	public function createJSON(array $datas):string {
-		$end=json_encode($datas);
-		return is_bool($end)? 'JSON Encode Error: '.$this->getJSONErrCode(json_last_error()): $end;
+	public function createJSON(array $datas): string {
+		$end = json_encode($datas);
+		return is_bool($end) ? 'JSON Encode Error: ' . $this->getJSONErrCode(json_last_error()) : $end;
 	}
 	
 	/**
@@ -36,21 +39,39 @@ class Translator {
 	/**
 	 * public string function createXML(array $datas)
 	 */
-	public function createXML(array $datas):string {
-		//
+	public function createXML(array $datas, DOMDocument $doc = null, DOMElement $node = null, bool $cdata = true): string {
+		if(is_null($doc)) $doc = new DOMDocument('1.0', 'utf-8');
+		if(is_null($node)){
+			$node = $doc->createElement('xml');
+			$doc->appendChild($node);
+		}
+		foreach($datas as $key => $data){
+			$childNode = $doc->createElement(is_string($key) ? $key : 'node');
+			$node->appendChild($childNode);
+			if(is_array($data)){
+				$this->createXML($data, $doc, $childNode, $cdata);
+			}else
+				$inner = $cdata ? $doc->createCDATASection($data) : $doc->createTextNode($data);
+			$childNode->appendChild($inner);
+		}
+		$end = $doc->saveXML();
+		return is_string($end) ? $end : '';
 	}
 	
 	/**
 	 * public array function parseXML(string $xml)
 	 */
-	public function parseXML(string $xml):array {
-		//
+	public function parseXML(string $xml): array {
+		libxml_disable_entity_loader(true);
+		
+		$xmlstring = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
+		
+		$val = json_decode(json_encode($xmlstring),true);
 	}
 	
 	/**
-	 * 
 	 */
-	protected function getJSONErrCode(int $errNo):string {
+	protected function getJSONErrCode(int $errNo): string {
 		switch($errNo){
 			case JSON_ERROR_NONE:
 				return 'JSON_ERROR_NONE';
