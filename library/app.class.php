c0a6b93795430028c04dc7782b7c73b8c04cb3dd
profoundblue <profoundblue@163.com>
2017-04-24 20:44:11 +0800

    0007++

--- library/app.class.php
+++ library/app.class.php
@@ -1,24 +1,56 @@
 <?php
 // declare(strict_types = 1);
 namespace SporeAura\WX;
-
+use PDO;
+use PDOException;
 class App {
+	/**
+	 * 
+	 */
+	protected $mysql;
 	
 	/**
 	 * public void function __construct(void)
 	 */
 	public function __construct() {
-		//
+		$dsn='mysql:host=127.0.0.1;dbname=wechat_ss_database;port=3306;charset=utf8';
+		$username='root';
+		$password='goodwin@000';
+		try {
+			$this->mysql=new \PDO($dsn,$username,$password);
+		}catch(PDOException $e){
+			$this->mysql=null;
+		}
 	}
 	
 	/**
 	 * public void function __destruct(void)
 	 */
 	function __destruct() {
-		//
+		$this->mysql=null;
 	}
 	
+	/**
+	 * 
+	 */
+	public function setToken():bool{
+		
+	}
 	
+	/**
+	 * 
+	 */
+	public function getToken(): string {
+		$sql="select `app_access_token` from `app` limit 1";
+		$ds=$this->mysql->query($sql);
+		if($ds){
+			$assoc=$ds->fetch(PDO::FETCH_ASSOC);
+			if($assoc){
+				return $assoc['app_access_token'];
+			}
+		}
+		return null;
+	}
 	//
 }
 
